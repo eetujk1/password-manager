353b81ace7fadc50cc04164b16781a4e1da53449
eetujk1 <eetukanniainen123@gmail.com>
2026-08-04 13:26:25 +0300

    Update password manager UI

--- gui.py
+++ gui.py
@@ -1,67 +1,59 @@
 import customtkinter as ctk
-import generator
 
-class main_window(ctk.CTk):
+
+class MainWindow(ctk.CTk):
     def __init__(self):
         super().__init__()
 
-        self.setup()
-        self.create_frames()
-        self.layout_widgets()
+        self.title("Password manager")
+        self.geometry("600x500")
 
-    def setup(self):   
-        self.title("Password Manager")
-        self.geometry("500x400")
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-
-        self.mainframe = ctk.CTkFrame(self, fg_color="transparent")
-        self.mainframe.grid(row=0, column=0)
-        self.mainframe.columnconfigure(0, weight = 1)
-        self.mainframe.rowconfigure(0, weight = 1)
-
-    def create_frames(self):
-        
-        self.frame = ctk.CTkFrame(self.mainframe)  
-        self.frame.grid(row=0, column=0)
-        self.label = ctk.CTkLabel(
-            self.frame,
-            text="Password Manager"
-
+        self.websitelabel = ctk.CTkLabel(
+            self,
+            text="Website"
         )
-        self.frame.grid_columnconfigure(0, weight = 1)
-        self.frame.grid_rowconfigure(0, weight = 1)
 
-    def layout_widgets(self):
+        self.grid_columnconfigure(0, weight=1)
+        self.websitelabel.grid(row=0, column=0, padx = 20, pady = 10, sticky ="w")
 
-        self.label.grid(row = 0, column = 0)  
-        
-        self.length_entry = ctk.CTkEntry(self.frame, placeholder_text = "Enter password")
-        self.length_entry.grid(row = 1, column = 0)
+
+        self.website_entry = ctk.CTkEntry(self, placeholder_text = "Website")
+        self.website_entry.grid(row=1, column=0, padx = 20, pady = 5, sticky ="ew")
+
+        self.usernameLabel = ctk.CTkLabel(self, text="Username")
+
+        self.usernameLabel.grid(row=2, column=0, padx = 20, pady = 5, sticky ="w")  
+
+        self.username_entry = ctk.CTkEntry(self, placeholder_text = "Username")
+        self.username_entry.grid(row=3, column=0, padx = 20, pady = 5, sticky ="ew")
+
+        self.passwordLabel = ctk.CTkLabel(self, text="Password")
+        self.passwordLabel.grid(row=4, column=0, padx = 20, pady = 5, sticky ="w") 
+
+        self.password_entry = ctk.CTkEntry(self, placeholder_text = "Password" )
+        self.password_entry.grid(row=5, column=0, padx = 20, pady = 5, sticky ="ew")
+
+        self.button_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.button_frame.grid(row=6, column=0, pady=20)
+    
 
         self.generate_button = ctk.CTkButton(
-            self.frame,
-            text="Generate",
-            command=self.generate
-        )
+                    self.button_frame,
+                    text="Generate password",
+                
+                    )    
 
-        self.generate_button.grid(row=2, column=0, padx=20, pady=10)
+        self.generate_button.grid(row=6, column=0, padx=(0, 10), pady=20)
 
-        self.password_label = ctk.CTkLabel(
-        self.frame,
-        text="Generated password will appear here",
-        wraplength=500
-    )
-        self.password_label.grid(row=3, column=0, pady=(20, 10))
+        self.password_label = ctk.CTkLabel(self.button_frame, text = "")
+        self.password_label.grid(padx = 5)
 
-        
 
-    def generate(self):
-        length = self.length_entry.get()
-        password, chars, length = generator.generate_password(length)
-        self.password_label.configure(text=password)
+        self.save_button = ctk.CTkButton(self.button_frame, text = "Save")
+        self.save_button.grid(row=6, column=1)
+
 
 
 if __name__ == "__main__":
-    app = mainwindow()
+    app = MainWindow()
     app.mainloop()
